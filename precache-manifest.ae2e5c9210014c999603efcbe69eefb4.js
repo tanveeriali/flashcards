9dcc2349a0536185b454f1d01474164a1baee8ea
Tanveer <tanveera@uw.edu>
2020-06-14 14:26:23 -0700

    Updates

---
+++ precache-manifest.ae2e5c9210014c999603efcbe69eefb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6b74b56933f38ce6585859b4f912a",
+    "url": "/brainkwik/index.html"
+  },
+  {
+    "revision": "7c8e8340dc2247f696dc",
+    "url": "/brainkwik/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4fbfbd40d6e2bb87f0d6",
+    "url": "/brainkwik/static/css/main.c1897f62.chunk.css"
+  },
+  {
+    "revision": "7c8e8340dc2247f696dc",
+    "url": "/brainkwik/static/js/2.57f217e3.chunk.js"
+  },
+  {
+    "revision": "603d9a3c4cc5b514df9304cf7a18f352",
+    "url": "/brainkwik/static/js/2.57f217e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbfbd40d6e2bb87f0d6",
+    "url": "/brainkwik/static/js/main.116f9308.chunk.js"
+  },
+  {
+    "revision": "7a547b56410266a77eb5",
+    "url": "/brainkwik/static/js/runtime-main.3a1becb8.js"
+  }
+]);
